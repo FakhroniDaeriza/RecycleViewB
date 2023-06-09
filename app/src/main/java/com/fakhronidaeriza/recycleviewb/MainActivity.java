d2e8814384a6fb2f6c7d0eb4608224d80ce783cb
fakhroni <fakhroni.daeriza12@gmail.com>
2023-06-09 14:05:35 +0700

    RecycleBTampil

--- app/src/main/java/com/fakhronidaeriza/recycleviewb/MainActivity.java
+++ app/src/main/java/com/fakhronidaeriza/recycleviewb/MainActivity.java
@@ -1,14 +1,52 @@
 package com.fakhronidaeriza.recycleviewb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private RecyclerView _recyclerView1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
+
+        AsyncHttpClient ahc = new AsyncHttpClient();
+        String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
+        
+        ahc.get(url, new AsyncHttpResponseHandler()
+        {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
+            {
+                Gson g = new Gson();
+                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+                RecyclerView.LayoutManager lm = new LinearLayoutManager( MainActivity.this);
+                _recyclerView1.setLayoutManager(lm);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+                _recyclerView1.setAdapter(ma);
+            }
+
+            @Override
+            public void onFailure(int statusCode, PreferenceActivity.Header[] headers, byte[] responseBody, Throwable error)
+            {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
